eebe64d5c342704108be8ec01be7ebac05eca24e
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2024-02-26 21:41:11 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Main from "@/components/Main";
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen relative">
+      <Main />
+    </main>
+  );
+}
