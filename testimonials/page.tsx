c569dba87d16682534b4ec7b84b47f041ca81cd9
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2024-03-08 10:08:01 -0700

    Add files via upload

--- testimonials/page.tsx
+++ testimonials/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { FaQuoteLeft } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -19,11 +20,34 @@ const Reviews = () => {
       <Swiper slidesPerView={1} breakpoints= {{
         640: { slidesPerView: 2 },
         1400: { slidesPerView: 3},
-      }}>
+      }}
+      spaceBetween={30}
+      modules={[Pagination]}
+      pagination={{
+        clickable: true,
+      }}
+      className='h-[350px] mb-12 xl:mb-32'
+      >
             {Test.map((person, index) => {
               return (
                 <SwiperSlide key={index}>
-                  <div className="text-white">Card</div>
+                  <div className='text-white p-0 mb-10'>
+                    <div className="flex items-center gap-x-4">
+                      <Image 
+                        src={person.avatar}
+                        width={70}
+                        height={30}
+                        alt=""
+                        priority
+                        className="pix"
+                      />
+                      <div className="flex flex-col">
+                        <div>{person.name}</div>
+                        <p className="text-orange-400">{person.rating}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-white bg-tertiary dark:bg-secondary/40 p-8 min-h-[400px] text-lg text-muted-foreground"><FaQuoteLeft className="text-gray-500"/>{person.review}</div>
                 </SwiperSlide>
               )
             })}
